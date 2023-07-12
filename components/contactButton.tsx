128b0c33c9cf91cbb683caf828e35fcb868e8077
joaopinto <j.c.moreirapinto@gmail.com>
2023-07-12 22:08:13 +0100

    ts fix

--- components/contactButton.tsx
+++ components/contactButton.tsx
@@ -2,7 +2,7 @@ import { ArrowRightCircleIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
 import React from "react";
 
-const ContactButton = (props) => {
+const ContactButton = (props:  { base: string }) => {
   return (
     <Link href={`${props.base}/contact`} className="px-6 py-3 text-white bg-red-600 rounded-full">
         <div  style={{display: 'flex', flexDirection: 'row' }}>
