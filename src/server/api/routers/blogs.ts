c20ed764705edca456a70490ff0bd1c5fc84a49a
joaopinto <j.c.moreirapinto@gmail.com>
2023-07-13 08:38:00 +0100

    fix prisma

--- src/server/api/routers/blogs.ts
+++ src/server/api/routers/blogs.ts
@@ -4,6 +4,6 @@ import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const blogsRouter = createTRPCRouter({
   getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.blog.findMany();
+    return ctx.prisma.post.findMany();
   }),
 });
